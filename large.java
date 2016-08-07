d1967a9bd08b1ba49ca9d4a9d6b71aa2f615557d
GowthamiMurugesan <gowthamimurugesan333@gmail.com>
2016-08-07 11:34:19 +0530

    Add files via upload
    large

---
+++ large.java
@@ -0,0 +1,21 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class large {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a,b,c;
+	a=in.nextInt();
+	b=in.nextInt();
+	c=in.nextInt();
+	if(a>b&&a>c){
+		System.out.println("a is greater");
+	}
+	else if(b>a&&b>c){
+		System.out.println("b is greater");
+	}
+	else
+		System.out.println("c is greater");
+}
+}
